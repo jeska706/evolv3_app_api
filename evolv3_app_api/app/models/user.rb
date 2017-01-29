951aa49dcff2568dd4007542254c726d60c18b80
Jessica Beardsley <jessicabeardsley@hotmail.com>
2017-01-29 10:24:46 -0700

    Finalized assoc

--- evolv3_app_api/app/models/user.rb
+++ evolv3_app_api/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-    validates :name, :email, presence: true 
+    validates :name, :email, presence: true
+    has_many :creations
 end
--- evolv3_app_api/db/schema.rb
+++ evolv3_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129171255) do
+ActiveRecord::Schema.define(version: 20170129172236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,16 @@ ActiveRecord::Schema.define(version: 20170129171255) do
     t.string   "artist"
     t.string   "title"
     t.string   "img"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "users_count"
   end
 
   create_table "creations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
